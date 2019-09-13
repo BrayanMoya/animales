e1bfe66e0fb858c5abf85a480b22e16a1de97c9a
bmoya <bmoya17@hotmail.com>
2019-09-12 22:10:17 -0500

    ahora si

--- src/administrar_animal.php
+++ src/administrar_animal.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 //incluye la clase Animal y CrudAnimal
 require_once('crud_animal.php');
@@ -27,34 +26,4 @@ $animal= new Animal();
 	}elseif($_GET['accion']=='a'){
 		header('Location: actualizar.php');
 	}
-=======
-<?php
-//incluye la clase Animal y CrudAnimal
-require_once('crud_animal.php');
-require_once('animal.php');
- 
-$crud= new CrudAnimal();
-$animal= new Animal();
- 
-	// si el elemento insertar no viene nulo llama al crud e inserta un animal
-	if (isset($_POST['insertar'])) {
-		$animal->setNombre($_POST['nombre']);
-		//llama a la función insertar definida en el crud
-		$crud->insertar($animal);
-		header('Location: index.php');
-	// si el elemento de la vista con nombre actualizar no viene nulo, llama al crud y actualiza el animal
-	}elseif(isset($_POST['actualizar'])){
-		$animal->setId($_POST['id']);
-		$animal->setNombre($_POST['nombre']);
-		$crud->actualizar($animal);
-		header('Location: index.php');
-	// si la variable accion enviada por GET es == 'e' llama al crud y elimina un animal
-	}elseif ($_GET['accion']=='e') {
-		$crud->eliminar($_GET['id']);
-		header('Location: index.php');		
-	// si la variable accion enviada por GET es == 'a', envía a la página actualizar.php 
-	}elseif($_GET['accion']=='a'){
-		header('Location: actualizar.php');
-	}
->>>>>>> 3f6123803308b5e54fed5bf1490f64bb965ca909
 ?>
--- src/animal.php
+++ src/animal.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	class Animal{
 		private $id;
@@ -22,29 +21,4 @@
 			$this->id = $id;
 		}
 	}
-=======
-<?php
-	class Animal{
-		private $id;
-		private $nombre;
- 
-		function __construct(){}
- 
-		public function getNombre(){
-			return $this->nombre;
-		}
- 
-		public function setNombre($nombre){
-			$this->nombre = $nombre;
-		}
- 
-		public function getId(){
-			return $this->id;
-		}
- 
-		public function setId($id){
-			$this->id = $id;
-		}
-	}
->>>>>>> 3f6123803308b5e54fed5bf1490f64bb965ca909
 ?>
