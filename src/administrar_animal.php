1aba191debb2eac3defd8ad41f0a3337302bfa11
bmoya <bmoya17@gotmail.com>
2020-06-20 19:58:38 -0500

    Codigo oloroso intencional solucionado

--- src/administrar_animal.php
+++ src/administrar_animal.php
@@ -5,24 +5,24 @@ require_once('animal.php');
  
 $crud= new CrudAnimal();
 $animal= new Animal();
-define("Location: index.php", "localizacionIndex");
+define("LOCINDEX", "Location: index.php");
  
 	// si el elemento insertar no viene nulo llama al crud e inserta un animal
 	if (isset($_POST['insertar'])) {
 		$animal->setNombre($_POST['nombre']);
 		//llama a la función insertar definida en el crud
 		$crud->insertar($animal);
-		header(localizacionIndex);
+		header(LOCINDEX);
 	// si el elemento de la vista con nombre actualizar no viene nulo, llama al crud y actualiza el animal
 	}elseif(isset($_POST['actualizar'])){
 		$animal->setId($_POST['id']);
 		$animal->setNombre($_POST['nombre']);
 		$crud->actualizar($animal);
-		header(localizacionIndex);
+		header(LOCINDEX);
 	// si la variable accion enviada por GET es == 'e' llama al crud y elimina un animal
 	}elseif ($_GET['accion']=='e') {
 		$crud->eliminar($_GET['id']);
-		header(localizacionIndex);		
+		header(LOCINDEX);		
 	// si la variable accion enviada por GET es == 'a', envía a la página actualizar.php 
 	}elseif($_GET['accion']=='a'){
 		header('Location: actualizar.php');
