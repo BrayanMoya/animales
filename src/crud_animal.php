58060c6b6676fcd559f1cdc91e3762ca548bf5b5
bmoya <bmoya17@gotmail.com>
2020-08-10 21:43:10 -0500

    codigo oloroso solucionado

--- src/crud_animal.php
+++ src/crud_animal.php
@@ -2,6 +2,7 @@
 // incluye la clase Db
 require_once('conexion.php');
 
+const NOMBRE = 'nombre'
  
 	class CrudAnimal{
 		
@@ -12,7 +13,7 @@ require_once('conexion.php');
 		public function insertar($animal){
 			$db=Db::conectar();
 			$insert=$db->prepare('INSERT INTO animales values(NULL,:nombre)');
-			$insert->bindValue('nombre',$animal->getNombre());
+			$insert->bindValue(NOMBRE,$animal->getNombre());
 			$insert->execute();
  
 		}
@@ -26,7 +27,7 @@ require_once('conexion.php');
 			foreach($select->fetchAll() as $animal){
 				$myanimal= new Animal();
 				$myanimal->setId($animal['id']);
-				$myanimal->setNombre($animal['nombre']);
+				$myanimal->setNombre($animal[NOMBRE]);
 				$listaanimales[]=$myanimal;
 			}
 			return $listaanimales;
@@ -57,7 +58,7 @@ require_once('conexion.php');
 			$db=Db::conectar();
 			$actualizar=$db->prepare('UPDATE animales SET nombre=:nombre WHERE ID=:id');
 			$actualizar->bindValue('id',$animal->getId());
-			$actualizar->bindValue('nombre',$animal->getNombre());
+			$actualizar->bindValue(NOMBRE,$animal->getNombre());
 			$actualizar->execute();
 		}
 	}
